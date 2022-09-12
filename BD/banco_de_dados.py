3f6026e8b586869d25664d5e45a3765a1538427c
Andre de Paula <andrepps22@gmail.com>
2022-09-12 13:10:39 -0300

    ''

--- BD/banco_de_dados.py
+++ BD/banco_de_dados.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, Column, String, Integer, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from logins import login, senha
+from .logins import login, senha
 
 engine = create_engine(
     f'mysql+pymysql://{login}:{senha}@localhost:3306/falecidos')
