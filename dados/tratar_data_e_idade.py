65e5201778124120e1a741799931237ed536e02a
Andre de Paula <andrepps22@gmail.com>
2022-09-14 14:49:56 -0300

    Resolvido problema com o mês de março que estava sem o 'ç',

--- dados/tratar_data_e_idade.py
+++ dados/tratar_data_e_idade.py
@@ -2,16 +2,20 @@ import locale
 from datetime import datetime
 
 
-def formatar_data(date):
+def formatar_data(date: str):
     '''
         Recebe uma data por ex: 'sexta-feira, 9 de outubro de 2022' e retorna 09-10-2022
     '''
-    locale.setlocale(locale.LC_ALL, 'pt_BR.utf8')
+    locale.setlocale(locale.LC_ALL, 'pt_BR')
     data = date
     data_nova = data.split()
+    if data_nova[3] == "marco":
+        data_nova.remove('marco')
+        data_nova.insert(3, 'março')
 
     data_tratada = datetime.strptime(
         f'{data_nova[1]}-{data_nova[3]}-{data_nova[5]}', '%d-%B-%Y')
+
     return data_tratada.date()
 
 
@@ -24,3 +28,7 @@ def formatar_idade(idade: str):
         if idade_tratada.isnumeric():
 
             return int((idade_tratada))
+
+
+if __name__ == '__main__':
+    print(formatar_data('q 13 de marco de 2022'))
